57b8bcdb5a8dce406abe4dbe891f1c1357d3def4
Emil Björnlinger <15017341+emilbjornlinger@users.noreply.github.com>
2024-01-12 12:37:48 +0100

    Finished day 5 part 1, started part 2

--- days/day5/puzzle.go
+++ days/day5/puzzle.go
@@ -5,9 +5,25 @@ import (
     "os"
     "path/filepath"
     "aoc2023/input"
+    "strings"
+    "strconv"
 )
 
 const dayName string = "day5"
+const MaxInt = int((^uint(0)) >> 1)
+
+type Range struct {
+    start int
+    end int
+    outputStart int
+}
+
+type CatRange struct {
+    start int
+    end int
+}
+
+// 279289190 too high
 
 func Puzzle1() {
     wd, err  := os.Getwd()
@@ -17,13 +33,63 @@ func Puzzle1() {
     filename := filepath.Join(wd, "days", dayName, "input.txt")
     inputSlice := input.GetInputSlice(filename)
 
-    // Implementation
-    for _, line := range inputSlice {
-        fmt.Println(line)
+    mappings := []string{"seed-to-soil", "soil-to-fertilizer", "fertilizer-to-water", "water-to-light", "light-to-temperature", "temperature-to-humidity", "humidity-to-location"}
+
+    // Create and populate ranges for each mapping
+    seedToSoil := make([]Range, 0)
+    populate(&seedToSoil, "seed-to-soil", inputSlice)
+
+    soilToFertilizer := make([]Range, 0)
+    populate(&soilToFertilizer, "soil-to-fertilizer", inputSlice)
+
+    fertilizerToWater := make([]Range, 0)
+    populate(&fertilizerToWater, "fertilizer-to-water", inputSlice)
+
+    waterToLight := make([]Range, 0)
+    populate(&waterToLight, "water-to-light", inputSlice)
+
+    lightToTemperature := make([]Range, 0)
+    populate(&lightToTemperature, "light-to-temperature", inputSlice)
+
+    temperatureToHumidity := make([]Range, 0)
+    populate(&temperatureToHumidity, "temperature-to-humidity", inputSlice)
+
+    humidityToLocation := make([]Range, 0)
+    populate(&humidityToLocation, "humidity-to-location", inputSlice)
+
+    mappingsToRange := make(map[string][]Range)
+    mappingsToRange["seed-to-soil"] = seedToSoil
+    mappingsToRange["soil-to-fertilizer"] = soilToFertilizer
+    mappingsToRange["fertilizer-to-water"] = fertilizerToWater
+    mappingsToRange["water-to-light"] = waterToLight
+    mappingsToRange["light-to-temperature"] = lightToTemperature
+    mappingsToRange["temperature-to-humidity"] = temperatureToHumidity
+    mappingsToRange["humidity-to-location"] = humidityToLocation
+
+    // Extract seed numbers
+    seedsString := strings.Split(inputSlice[0], " ")
+    seedsString = append([]string(nil), seedsString[1:]...)
+    seeds := make([]int, 0)
+    for _, seed := range seedsString {
+        seedNum, _ := strconv.Atoi(seed)
+        seeds = append(seeds, seedNum)
     }
 
-    output := "Hello from " + dayName
-    fmt.Printf("Output: %v\n", output)
+    lowestLocation := MaxInt
+    for i, seed := range seeds {
+        input := seed
+        for _, mapping := range mappings {
+            input = transform(mappingsToRange[mapping], input)
+        }
+
+        fmt.Printf("output for seed %v: %v\n", i, input)
+
+        if input < lowestLocation {
+            lowestLocation = input
+        }
+    }
+
+    fmt.Printf("Output: %v\n", lowestLocation)
 }
 
 func Puzzle2() {
@@ -31,14 +97,138 @@ func Puzzle2() {
     if err != nil {
         panic (err)
     }
-    filename := filepath.Join(wd, "days", dayName, "input.txt")
+    filename := filepath.Join(wd, "days", dayName, "test.txt")
     inputSlice := input.GetInputSlice(filename)
 
-    // Implementation
-    for _, line := range inputSlice {
-        fmt.Println(line)
+    //mappings := []string{"seed-to-soil", "soil-to-fertilizer", "fertilizer-to-water", "water-to-light", "light-to-temperature", "temperature-to-humidity", "humidity-to-location"}
+
+    // Create and populate ranges for each mapping
+    seedToSoil := make([]Range, 0)
+    populate(&seedToSoil, "seed-to-soil", inputSlice)
+
+    soilToFertilizer := make([]Range, 0)
+    populate(&soilToFertilizer, "soil-to-fertilizer", inputSlice)
+
+    fertilizerToWater := make([]Range, 0)
+    populate(&fertilizerToWater, "fertilizer-to-water", inputSlice)
+
+    waterToLight := make([]Range, 0)
+    populate(&waterToLight, "water-to-light", inputSlice)
+
+    lightToTemperature := make([]Range, 0)
+    populate(&lightToTemperature, "light-to-temperature", inputSlice)
+
+    temperatureToHumidity := make([]Range, 0)
+    populate(&temperatureToHumidity, "temperature-to-humidity", inputSlice)
+
+    humidityToLocation := make([]Range, 0)
+    populate(&humidityToLocation, "humidity-to-location", inputSlice)
+
+    mappingsToRange := make(map[string][]Range)
+    mappingsToRange["seed-to-soil"] = seedToSoil
+    mappingsToRange["soil-to-fertilizer"] = soilToFertilizer
+    mappingsToRange["fertilizer-to-water"] = fertilizerToWater
+    mappingsToRange["water-to-light"] = waterToLight
+    mappingsToRange["light-to-temperature"] = lightToTemperature
+    mappingsToRange["temperature-to-humidity"] = temperatureToHumidity
+    mappingsToRange["humidity-to-location"] = humidityToLocation
+
+    ranges := make([]CatRange, 0)
+
+    // Extract seed numbers into slice of ranges
+    extractSeedRanges(&ranges, inputSlice[0])
+
+    for _, r := range ranges {
+        r.Print()
     }
 
-    output := "Hello from " + dayName
-    fmt.Printf("Output: %v\n", output)
+    lowestLocation := MaxInt
+    //for i, seed := range seeds {
+    //    input := seed
+    //    for _, mapping := range mappings {
+    //        input = transform(mappingsToRange[mapping], input)
+    //    }
+
+    //    fmt.Printf("output for seed %v: %v\n", i, input)
+
+    //    if input < lowestLocation {
+    //        lowestLocation = input
+    //    }
+    //}
+
+    // Extract lowest location from ranges
+
+    fmt.Printf("Output: %v\n", lowestLocation)
+}
+
+func populate(rangeList *[]Range, mapping string, input []string) {
+    parsing := false
+
+    index := 0
+    for true {
+        if strings.Contains(input[index], mapping) {
+            parsing = true
+            index++
+            continue
+        }
+
+        if parsing {
+            if (input[index] == "") {
+                break
+            }
+
+            numStrings := strings.Split(input[index], " ")
+            
+            start, _ := strconv.Atoi(numStrings[1])
+            rangeLength, _ := strconv.Atoi(numStrings[2])
+            end := start + rangeLength - 1
+            outputStart, _ := strconv.Atoi(numStrings[0])
+            newRange := Range{start: start, end: end, outputStart: outputStart}
+
+            (*rangeList) = append((*rangeList), newRange)
+        }
+
+        
+        index++
+        if index == len(input) {
+            break
+        }
+    }
+}
+
+func transform(rangeList []Range, input int) int {
+    // Loop through all ranges and see if match
+    for _, currRange := range rangeList {
+        if currRange.start <= input && input <= currRange.end {
+            fmt.Printf("output: %v\n", input)
+            return currRange.outputStart + (input - currRange.start)
+        }
+    }
+
+    // Otherwise return the input
+    fmt.Printf("output: %v\n", input)
+    return input
+}
+
+func extractSeedRanges(rangeList *[]CatRange, input string) {
+
+    seedsString := strings.Split(input, " ")
+    seedsString = append([]string(nil), seedsString[1:]...)
+    for i := 0; i < len(seedsString)-1; i++ {
+        if i % 2 == 0 {
+            start, _ := strconv.Atoi(seedsString[i])
+            length, _ := strconv.Atoi(seedsString[i+1])
+            end := start + length - 1
+            newRange := CatRange{start: start, end: end}
+            (*rangeList) = append((*rangeList), newRange)
+        }
+    }
+}
+
+func (r *Range) Print() {
+    fmt.Printf("\nstart: %v\nend: %v\noutputStart: %v\n", r.start, r.end, r.outputStart)
+}
+
+func (r *CatRange) Print() {
+    fmt.Printf("\nstart: %v\nend: %v\n", r.start, r.end)
 }
